1287223726599377f374cc7f5ab4917c7253e3bc
Akaikenlol <keithken972@gmail.com>
2023-09-09 22:33:32 +0630

    comment out the clerkprovider tag in auth onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -17,17 +17,17 @@ async function Page() {
 
 	return (
 		<>
-			<ClerkProvider>
-				<main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-					<h1 className="head-text">Onboarding</h1>
-					<p className="mt-3 text-base-regular text-light-2">
-						Complete your profile to use Threads.
-					</p>
-					<section className="mt-9 p-10 bg-dark-2">
-						<AccountProfile user={userData} btnTitle="Continue" />
-					</section>
-				</main>
-			</ClerkProvider>
+			{/* <ClerkProvider> */}
+			<main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+				<h1 className="head-text">Onboarding</h1>
+				<p className="mt-3 text-base-regular text-light-2">
+					Complete your profile to use Threads.
+				</p>
+				<section className="mt-9 p-10 bg-dark-2">
+					<AccountProfile user={userData} btnTitle="Continue" />
+				</section>
+			</main>
+			{/* </ClerkProvider> */}
 		</>
 	);
 }
